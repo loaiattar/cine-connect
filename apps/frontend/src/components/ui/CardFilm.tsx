6bcc5093e8399ebc55c3c90b96af41ceb33abc5b
yaacov-zakine <kikiyaacov@icloud.com>
2026-02-21 21:28:49 +0100

    feat: add bottom dark gradient overlay

--- apps/frontend/src/components/ui/CardFilm.tsx
+++ apps/frontend/src/components/ui/CardFilm.tsx
@@ -35,6 +35,9 @@ export default function MovieCard({
         className="w-full h-full object-cover"
       />
 
+     
+      <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black to-transparent" />
+
     </div>
   )
 }
