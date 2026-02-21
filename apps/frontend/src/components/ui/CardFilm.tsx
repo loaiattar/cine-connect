6a705f92c3f9056343eb95a97134e6ffe5f814fa
yaacov-zakine <kikiyaacov@icloud.com>
2026-02-21 21:17:17 +0100

    feat: add hovered state

--- apps/frontend/src/components/ui/CardFilm.tsx
+++ apps/frontend/src/components/ui/CardFilm.tsx
@@ -1,17 +1,30 @@
-// props du composant
+import { useState } from "react";
+
 interface MovieCardProps {
-  id: number
-  title: string
-  year: number
-  rating: number
-  imageUrl: string
-  genres: string[] // ex: ["Action", "Aventure"]
+  id: number;
+  title: string;
+  year: number;
+  rating: number;
+  imageUrl: string;
+  genres: string[]; // ex: ["Action", "Aventure"]
 }
 
 // carte d'un film
-export default function MovieCard({ id: _id, title: _title, year: _year, rating: _rating, imageUrl: _imageUrl, genres: _genres }: MovieCardProps) {
+export default function MovieCard({
+  id: _id,
+  title: _title,
+  year: _year,
+  rating: _rating,
+  imageUrl: _imageUrl,
+  genres: _genres,
+}: MovieCardProps) {
+
+  // est-ce que la souris est sur la carte
+  const [hovered, setHovered] = useState(false)
+
+  console.log(hovered) // temporaire
+
   return (
- 
     <div className="relative rounded-xl overflow-hidden w-full">
 
     </div>
