911161d98c76c00b514de9746663638d0b76f44f
loaiattar <loaiattar@gmail.com>
2026-02-22 05:41:29 +0100

    test: setup vitest and supertest with the movie.test.ts

--- apps/backend/src/routes/movie.route.ts
+++ apps/backend/src/routes/movie.route.ts
@@ -1,27 +1,28 @@
 import { Router } from "express";
 import { MovieController } from "../controllers/movie.controller";
+import { authMiddleware } from "../middlewares/auth.middleware";
 
 const router: Router = Router();
 
 // GET /api/movies/:imdbId
-router.get("/:imdbId", MovieController.getMovieDetails);
+router.get("/:imdbId", authMiddleware, MovieController.getMovieDetails);
 // POST /api/movies/favorite
-router.post("/favorite", MovieController.handleToggleFavorite);
+router.post("/favorite", authMiddleware, MovieController.handleToggleFavorite);
 // GET /api/movies/favorites/:userId
-router.get("/favorites/:userId", MovieController.getUserFavorites);
+router.get("/favorites/:userId", authMiddleware, MovieController.getUserFavorites);
 // POST /api/movies/watchlist
-router.post("/watchlist", MovieController.addToWatchlist);
+router.post("/watchlist", authMiddleware, MovieController.addToWatchlist);
 // GET /api/movies/comments/:movieId
-router.get("/comments/:movieId", MovieController.getMovieComments);
+router.get("/comments/:movieId", authMiddleware, MovieController.getMovieComments);
 // POST /api/movies/comments
-router.post("/comments", MovieController.addComment);
+router.post("/comments", authMiddleware, MovieController.addComment);
 // DELETE /api/movies/comments
-router.delete("/comments", MovieController.deleteComment);
+router.delete("/comments", authMiddleware, MovieController.deleteComment);
 // PUT /api/movies/comments
-router.put("/comments", MovieController.updateComment);
+router.put("/comments", authMiddleware, MovieController.updateComment);
 // GET /api/movies/watchlist/:userId
-router.get("/watchlist/:userId", MovieController.getMovieWatchlist);
+router.get("/watchlist/:userId", authMiddleware, MovieController.getMovieWatchlist);
 // DELETE /api/movies/watchlist
-router.delete("/watchlist", MovieController.deleteMovieFromWatchlist);
+router.delete("/watchlist", authMiddleware, MovieController.deleteMovieFromWatchlist);
 
 export default router;
