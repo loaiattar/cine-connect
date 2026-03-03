c7072479adbf9018a6f723f52ceac5ba8d27510c
loaiattar <loaiattar@gmail.com>
2026-03-03 12:12:19 +0100

    add follow controller and follow route in user route

--- apps/backend/src/routes/user.route.ts
+++ apps/backend/src/routes/user.route.ts
@@ -1,13 +1,17 @@
 import { Router } from "express";
 import { UserController } from "../controllers/user.controller";
+import { FollowController } from "../controllers/follow.controller";
 import { authMiddleware } from "../middlewares/auth.middleware";
 import { asyncHandler } from "../middlewares/errorHandler.middleware";
 import { validate } from "../middlewares/validation.middleware";
 import { updateProfileSchema } from "../schemas/user.schema";
+import { getFollowersSchema, getFollowingSchema } from "../schemas/follow.schema";
 
 const router: Router = Router();
 
 router.get("/me", authMiddleware, asyncHandler(UserController.getMe));
 router.put("/me", authMiddleware, validate(updateProfileSchema), asyncHandler(UserController.updateMe));
+router.get("/:userId/followers", validate(getFollowersSchema), asyncHandler(FollowController.getFollowers));
+router.get("/:userId/following", validate(getFollowingSchema), asyncHandler(FollowController.getFollowing));
 
 export default router;
