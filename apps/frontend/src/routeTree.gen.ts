986f86c7f6ca681a8e3663bd02d20c3bba4fff20
loaiattar <loaiattar@gmail.com>
2026-03-03 15:22:38 +0100

    moviemovie adding route

--- apps/frontend/src/routeTree.gen.ts
+++ apps/frontend/src/routeTree.gen.ts
@@ -16,6 +16,7 @@ import { Route as RegisterPageRouteImport } from './routes/RegisterPage'
 import { Route as MovieDetailPageRouteImport } from './routes/MovieDetailPage'
 import { Route as LoginPageRouteImport } from './routes/LoginPage'
 import { Route as IndexRouteImport } from './routes/index'
+import { Route as MovieMovieIdRouteImport } from './routes/movie.$movieId'
 
 const Login1Route = Login1RouteImport.update({
   id: '/login1',
@@ -52,6 +53,11 @@ const IndexRoute = IndexRouteImport.update({
   path: '/',
   getParentRoute: () => rootRouteImport,
 } as any)
+const MovieMovieIdRoute = MovieMovieIdRouteImport.update({
+  id: '/movie/$movieId',
+  path: '/movie/$movieId',
+  getParentRoute: () => rootRouteImport,
+} as any)
 
 export interface FileRoutesByFullPath {
   '/': typeof IndexRoute
@@ -61,6 +67,7 @@ export interface FileRoutesByFullPath {
   '/ancienne-page': typeof AnciennePageRoute
   '/favorites': typeof FavoritesRoute
   '/login1': typeof Login1Route
+  '/movie/$movieId': typeof MovieMovieIdRoute
 }
 export interface FileRoutesByTo {
   '/': typeof IndexRoute
@@ -70,6 +77,7 @@ export interface FileRoutesByTo {
   '/ancienne-page': typeof AnciennePageRoute
   '/favorites': typeof FavoritesRoute
   '/login1': typeof Login1Route
+  '/movie/$movieId': typeof MovieMovieIdRoute
 }
 export interface FileRoutesById {
   __root__: typeof rootRouteImport
@@ -80,6 +88,7 @@ export interface FileRoutesById {
   '/ancienne-page': typeof AnciennePageRoute
   '/favorites': typeof FavoritesRoute
   '/login1': typeof Login1Route
+  '/movie/$movieId': typeof MovieMovieIdRoute
 }
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
@@ -91,6 +100,7 @@ export interface FileRouteTypes {
     | '/ancienne-page'
     | '/favorites'
     | '/login1'
+    | '/movie/$movieId'
   fileRoutesByTo: FileRoutesByTo
   to:
     | '/'
@@ -100,6 +110,7 @@ export interface FileRouteTypes {
     | '/ancienne-page'
     | '/favorites'
     | '/login1'
+    | '/movie/$movieId'
   id:
     | '__root__'
     | '/'
@@ -109,6 +120,7 @@ export interface FileRouteTypes {
     | '/ancienne-page'
     | '/favorites'
     | '/login1'
+    | '/movie/$movieId'
   fileRoutesById: FileRoutesById
 }
 export interface RootRouteChildren {
@@ -119,6 +131,7 @@ export interface RootRouteChildren {
   AnciennePageRoute: typeof AnciennePageRoute
   FavoritesRoute: typeof FavoritesRoute
   Login1Route: typeof Login1Route
+  MovieMovieIdRoute: typeof MovieMovieIdRoute
 }
 
 declare module '@tanstack/react-router' {
@@ -172,6 +185,13 @@ declare module '@tanstack/react-router' {
       preLoaderRoute: typeof IndexRouteImport
       parentRoute: typeof rootRouteImport
     }
+    '/movie/$movieId': {
+      id: '/movie/$movieId'
+      path: '/movie/$movieId'
+      fullPath: '/movie/$movieId'
+      preLoaderRoute: typeof MovieMovieIdRouteImport
+      parentRoute: typeof rootRouteImport
+    }
   }
 }
 
@@ -183,6 +203,7 @@ const rootRouteChildren: RootRouteChildren = {
   AnciennePageRoute: AnciennePageRoute,
   FavoritesRoute: FavoritesRoute,
   Login1Route: Login1Route,
+  MovieMovieIdRoute: MovieMovieIdRoute,
 }
 export const routeTree = rootRouteImport
   ._addFileChildren(rootRouteChildren)
