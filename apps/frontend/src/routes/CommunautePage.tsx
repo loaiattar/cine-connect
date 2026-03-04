e4ea18cf1c29408f75794311fbb593b322647f58
yaacov-zakine <kikiyaacov@icloud.com>
2026-03-04 19:02:14 +0200

    feat:import

---
+++ apps/frontend/src/routes/CommunautePage.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import CineConnectNavbar from "../components/ui/Navbar";
+import CommunauteSection from "../components/ui/CommunauteSection";
+import { UserProfileCard } from "../components/ui/ProfileCard1";
+
+const CommunautePage = () => {
+  return (
+    <div style={{ backgroundColor: "#0d0d0d", minHeight: "100vh", color: "white" }}>
+      Communauté Page
+    </div>
+  );
+};
+
+export default CommunautePage;
